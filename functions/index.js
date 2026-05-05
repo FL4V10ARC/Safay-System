92bc454e6355adb9bcc80187622e99e6824a01d9
FL4V10ARC <flaviocarvalho9029@gmail.com>
2026-05-05 19:11:06 -0400

    feat: add admin order detail retrieval

--- functions/index.js
+++ functions/index.js
@@ -2,8 +2,10 @@ const {confirmOrder} = require("./orders/confirmOrder");
 const {cancelOrder} = require("./orders/cancelOrder");
 const {updateOrderStatus} = require("./orders/updateOrderStatus");
 const {getOrders} = require("./orders/getOrders");
+const {getOrderById} = require("./orders/getOrderById");
 
 exports.confirmOrder = confirmOrder;
 exports.cancelOrder = cancelOrder;
 exports.updateOrderStatus = updateOrderStatus;
 exports.getOrders = getOrders;
+exports.getOrderById = getOrderById;
---
+++ functions/orders/getOrderById.js
@@ -0,0 +1,28 @@
+const {onCall, HttpsError} = require("firebase-functions/v2/https");
+const {db} = require("../config/firebase");
+const {validateAdmin} = require("../utils/auth");
+
+exports.getOrderById = onCall(async (request) => {
+  const {orderId} = request.data;
+
+  await validateAdmin(request);
+
+  if (!orderId) {
+    throw new HttpsError("invalid-argument", "O ID do pedido é obrigatório.");
+  }
+
+  const orderRef = db.collection("orders").doc(orderId);
+  const orderSnap = await orderRef.get();
+
+  if (!orderSnap.exists) {
+    throw new HttpsError("not-found", "Pedido não encontrado.");
+  }
+
+  return {
+    success: true,
+    order: {
+      id: orderSnap.id,
+      ...orderSnap.data(),
+    },
+  };
+});
